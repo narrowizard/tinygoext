01002f2507b66bf55d29568389572d7d1e158d50
narro <501xs05@163.com>
2017-08-29 10:31:57 +0800

    rename package

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# tinygo-redis-session
-redis session support for tinygo.
+# tinygoext
+tinygo extension package.
++ redis session support for tinygo.
+
+## usage
+```go
+import (
+    "github.com/narrowizard/tinygoext/sessionext"
+    "github.com/kdada/tinygo/session"
+)
+
+session.Register("redis",sessionext.NewRediSessionContainer)
+```
+
+## config
+config in tinygo web.cfg  
++ SessionType redis
++ SessionSource 
--- sessionext/redis.go
+++ sessionext/redis.go
@@ -1,9 +1,21 @@
-package tinygoRedisSession
+package session
 
-import "github.com/kdada/tinygo/session"
+import (
+	"github.com/garyburd/redigo/redis"
+	"github.com/kdada/tinygo/session"
+	"github.com/kdada/tinygo/util"
+)
 
 type RediSession struct {
-	sessionId string
+	sessionId string                 // 会话id
+	data      map[string]interface{} // 数据
+}
+
+func newRediSession(sessionId string) *RediSession {
+	var ss = new(RediSession)
+	ss.sessionId = sessionId
+	ss.data = make(map[string]interface{}, 0)
+	return ss
 }
 
 func (this *RediSession) SessionId() string {
@@ -71,22 +83,43 @@ func (this *RediSession) Dead() bool {
 }
 
 type RediSessionContainer struct {
-	defaultExpire int    // 默认过期时间
-	source        string // host地址
-	closed        bool   //是否关闭
+	sessionCounter int                     // session计数器
+	sessions       map[string]*RediSession // 存储Session
+	defaultExpire  int                     // 默认过期时间
+	source         string                  // redis配置信息
+	closed         bool                    // 是否关闭
+	pool           *redis.Pool             // redis连接池
 }
 
 func NewRediSessionContainer(expire int, source string) (session.SessionContainer, error) {
 	var container = new(RediSessionContainer)
 	container.defaultExpire = expire
 	container.source = source
+
+	container.pool = &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			var conn, err = redis.Dial("tcp", source)
+			if err != nil {
+				return nil, err
+			}
+			return conn, nil
+		},
+	}
 	return container, nil
 }
 
 func (this *RediSessionContainer) CreateSession() (session.Session, bool) {
+	if this.closed {
+		return nil, false
+	}
+	var sessionId = util.NewUUID().Hex()
+	var ss = newRediSession(sessionId)
+	this.sessionCounter++
+	this.sessions[sessionId] = ss
 	return nil, false
 }
 
+// Session 获取Session,并且更新deadline,http processor 会在每次请求到的时候调用该方法
 func (this *RediSessionContainer) Session(sessionId string) (session.Session, bool) {
 	return nil, false
 }
